ce5100aa3790861b9345e0f742c55345579b3e75
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-09-24 10:58:35 +0300

    Hero scss (#20)
    * Update contacts.html
    
    * верстка contact+footer
    
    * Update contacts.html
    
    * made class for header
    
    * scss for header
    
    * dfjhsgfhj
    
    * do mobile-menu
    
    * do header and hero
    
    Co-authored-by: Serhii Vorotnoi <svorotnoy@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './js/modal.js'
+<<<<<<< HEAD
+const swiper = new Swiper(".mySwiper", {});
+import "./js/mobile-menu.js"
+
+=======
 import "./js/gallery-swiper"
 import "./js/reviews-swiper"
+>>>>>>> main
