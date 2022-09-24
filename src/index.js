ffe988d901c26812a88c699c5dfbbbbdf615f178
Nikita Kresik <51223547+Loki99911@users.noreply.github.com>
2022-09-24 11:12:09 +0300

    Reviews (#22)
    * rev03
    
    * svg
    
    * dsdsdsdsdsd
    
    * alot of
    
    * Lastcom
    
    * JS conflict

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import './js/modal.js'
-<<<<<<< HEAD
-const swiper = new Swiper(".mySwiper", {});
 import "./js/mobile-menu.js"
-
-=======
 import "./js/gallery-swiper"
 import "./js/reviews-swiper"
->>>>>>> main
+
