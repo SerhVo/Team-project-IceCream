33fab3c6fa183ec2671bb1d02b739ca5b19885eb
Loki99911 <51223547+Loki99911@users.noreply.github.com>
2022-09-24 08:12:38 +0000

    Deploying to gh-pages from @ Loki99911/team-project@ffe988d901c26812a88c699c5dfbbbbdf615f178 🚀

--- index.013e3000.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".gallerySwiper"),new Swiper(".mySwiper",{spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.013e3000.js.map
--- index.55d1627d.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".gallerySwiper"),new Swiper(".mySwiper",{spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.55d1627d.js.map
