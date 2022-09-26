07d07acde22a27b4215a6ea38d2ec69a4adcbbfa
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-09-26 21:51:54 +0300

    made all (#54)

--- src/js/buy-now.js
+++ src/js/buy-now.js
@@ -1,10 +1,11 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-buynow-open]"),
+    openedModalBtn: document.querySelector("[data-buymenu-open]"),
     closeModalBtn: document.querySelector("[data-buynow-close]"),
     modal: document.querySelector("[data-buynow]"),
   };
-
+  refs.openedModalBtn.addEventListener("click", toggleModal);
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
