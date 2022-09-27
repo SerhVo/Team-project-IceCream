14404527fdebe1abbd4646589eb1c4a723abee72
Serhii Vorotnoi <94438674+SerhVo@users.noreply.github.com>
2022-09-27 14:21:40 +0300

    Contacts+footer (#59)
    * доработки
    
    * добавив адреси
    
    * maps - modal done!
    
    * maps
    
    * fix
    
    fix

--- src/partials/js/modal.js
+++ src/partials/js/modal.js
@@ -1,20 +1,14 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open]"),
-      openedModalBtn: document.querySelector("[data-coffee-open]"),
-      openingModalBtn: document.querySelector("[data-milkshakes-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.openedModalBtn.addEventListener("click", toggleModal);
-    refs.openingModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
   })();
-
- 
