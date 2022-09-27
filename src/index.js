6550b3bc83dcad6e4c61a46c27ac737f55b3800b
TanitaL <111281242+TanitaL@users.noreply.github.com>
2022-09-27 14:10:33 +0200

    Modal (#63)
    * Create icons.svg
    
    * Update about.html
    
    * Update modal
    
    Co-authored-by: Nikita Kresik <51223547+Loki99911@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import "./js/mobile-menu.js"
 import "./js/gallery-swiper"
 import "./js/reviews-swiper"
 import "./js/buy-now"
+import "./js/readmore"
 import './js/modal-contacts.js'
 import './js/modal-fran.js'
